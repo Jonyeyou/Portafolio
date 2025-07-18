57a8ea3a178b0cfdf0349c7412821f7cebbcc41f
jonyeyou <jonathandemuria2708@gmail.com>
2025-07-18 15:04:11 -0300

    A

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header.tsx'
+import Repository from './components/Repository.tsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Repository />
     </>
   )
 }
---
+++ src/components/Repository.tsx
@@ -0,0 +1,9 @@
+
+const Repository = () => {
+    return (
+        <div>
+            <iframe src="https://github.com/Jonyeyou/Lubricentro-app.git" title="GitHub" width="100%" height="600px" />
+        </div>
+    );
+};
+export default Repository;
